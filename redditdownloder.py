490318520699ade17107ac3a96513cb4fe71facd
Yash Agarwal <yashagarwaljpr@gmail.com>
2017-12-23 16:12:04 +0530

    basic version working

--- redditdownloder.py
+++ redditdownloder.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
 import requests
-from bs4 import BeautifulSoup
+import praw
+import shutil
 
-# link = requests.get("https://www.reddit.com/r/EarthPorn/", stream=True)
-# print(link.content)
+def refineURL(url):
+    if url.endswith('png', 0, len(url)) or url.endswith('jpg', 0, len(url)):
+        return url
+    else:
+        return ""
 
-c = open("result", "r+")
-data = c.read()
-soup = BeautifulSoup(data)
+reddit = praw.Reddit('bot1', user_agent='testscript by /u/im_y')
 
-samples = soup.find_all("a", "title")
-print(samples)
+subreddit = reddit.subreddit('earthporn').hot(limit=10)
+# print(subreddit.__dict__.keys())
+
+downloadLinks = []
+for submission in subreddit:
+    downloadLinks.append(submission.url)
+
+len = len(downloadLinks)
+response = requests.get(downloadLinks[0], stream=True)
+with open('img' + '.' + 'jpg', 'wb') as out_file:
+    shutil.copyfileobj(response.raw, out_file)
+
+# print(downloadLinks)
